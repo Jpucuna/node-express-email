3cf7604fdc71ce96b89a7696c108576690920962
JPucuna <maximoportunista90@hotmail.com>
2023-09-06 22:44:40 -0500

    agregando valores del correo

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3977;
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
@@ -17,7 +18,6 @@ const transporter = nodemailer.createTransport({
     port: 465,
     secure: true,
     auth: {
-      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
       user: emailContainer,
       pass: pass
     }
@@ -28,22 +28,37 @@ app.get('/',(req, res)=>{
 })
 
 app.post('/enviar-correo', (req, res)=>{
-    const {email} = req.body; 
-    console.log(req.body);
+    const {email, name, subject, message} = req.body; 
+    const content = `<h3>
+    Te notificamos que has recibido el siguiente correo:
+    <br>
+    <br>
+        Nombre: ${name}
+    <br>
+        Email:  ${email}
+    <br>
+        Asunto: ${subject}
+    <br>
+    <br>
+
+        Mensaje:
+    <br>
+        ${message}
+    </h3>`
     const mailOptions = {
-        from: 'pruebitapah@gmail.com',
+        from: email,
         to: emailContainer,
-        subject: 'Asunto del correo',
-        text: 'Contenido de prueba',
+        subject: subject,
+        html: content,
       };
     
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
           console.error('Error al enviar el correo:', error);
-          res.status(500).send('Error al enviar el correo');
+          res.status(500).send({error:'Error al enviar el correo'});
         } else {
           console.log('Correo enviado con éxito:', info.response);
-          res.status(200).send('Correo enviado con éxito');
+          res.status(200).send({data:'Correo enviado con éxito'});
         }
       });
 });
